f71cd60aea5ae06f9d4c9d71022ae2b83d6b2c52
bmansk8 <bmansk8@yahoo.com>
2015-12-10 09:28:29 -0500

    moving []
    added a moving []

--- src/gamePanel.java
+++ src/gamePanel.java
@@ -7,6 +7,9 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
 
+	int player_x=10;
+	int player_y=10;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -23,18 +26,37 @@ public class GamePanel extends JPanel {
 		frame.setVisible(true);
 	}
 	
-	public GamePanel(){}
+	private GamePanel(){
+		
+		new Thread(){
+			public void run(){
+				try{
+					while(true){
+						gameLoop();
+						repaint();
+						Thread.sleep(33);
+					}
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}
+			}
+		}.start();
 
+	}
+
+	private void gameLoop(){
+		player_x++;
+		
+	}
+	
 	@Override
 	protected void paintComponent(Graphics g) {
 		g.setColor(Color.cyan);
 		g.fillRect(0, 0, getWidth(), getHeight());
 		
 		g.setColor(Color.black);
-		g.fillRect(10,20,50,50);
+		g.fillRect(player_x,player_y,50,50);
 		
-		g.setColor(Color.orange);
-		g.drawLine(60, 20, 90, 30);
 	}
 	
 	
